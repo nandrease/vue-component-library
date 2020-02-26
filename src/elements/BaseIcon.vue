0f614bebdc977a255a33c1147e1b4c6243c4a513
nandrease <neeme.andreas.eller@gmail.com>
2020-02-26 17:26:07 +0200

    BaseIcon and tests

---
+++ src/elements/BaseIcon.vue
@@ -0,0 +1,33 @@
+<template>
+    <svg class="icon" :class="`icon--${size}`" :style="styles">
+        <use :href="`${iconsUrl}#${icon}`"></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: ''
+        },
+        fill: {
+            type: String
+        }
+    },
+    computed: {
+        size() {
+            const num = this.icon.split('-').pop();
+            return isNaN(parseInt(num)) ? 16 : parseInt(num);
+        },
+        styles() {
+            return this.fill ? { fill: this.fill } : null;
+        },
+        iconsUrl() {
+            return process.env.VUE_APP_ICONS_URL || '';
+        }
+    }
+};
+</script>
+
+<style lang="scss"></style>
---
+++ tests/unit/BaseIcon.spec.ts
@@ -0,0 +1,44 @@
+import { shallowMount } from '@vue/test-utils';
+import BaseIcon from '@/elements/BaseIcon.vue';
+
+let wrapper: any = null;
+
+const propsData = {
+    icon: 'icon-search-16'
+};
+
+beforeEach(() => (wrapper = shallowMount(BaseIcon, { propsData })));
+afterEach(() => wrapper.destroy());
+
+describe('<ScoroIcon />', () => {
+    it('renders and displays properly', () => {
+        expect(wrapper.isVueInstance()).toBeTruthy();
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('has a use element with defined icon name and size', () => {
+        const useElem = wrapper.find('use');
+        expect(useElem.attributes('href')).toContain(propsData.icon);
+    });
+
+    it('prperly detects size from icon and uses it as a class on wrapper', () => {
+        expect(wrapper.classes()).toContain('icon--16');
+        wrapper = shallowMount(BaseIcon, {
+            propsData: {
+                icon: 'icon-search-24'
+            }
+        });
+        expect(wrapper.classes()).toContain('icon--24');
+    });
+
+    it('it uses fill prop to color the element', () => {
+        expect(wrapper.attributes().style).toBe(undefined);
+        wrapper = shallowMount(BaseIcon, {
+            propsData: {
+                ...propsData,
+                fill: 'rebeccapurple'
+            }
+        });
+        expect(wrapper.attributes().style).toBe('fill: rebeccapurple;');
+    });
+});
